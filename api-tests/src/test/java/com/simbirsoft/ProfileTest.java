dce5b2294639ea18ad93b009d8545dfee1088baa
AleksandrDankov <dankoffalexander@gmail.com>
2022-10-07 00:58:45 +0500

    Added simple grpc test

---
+++ api-tests/src/test/java/com/simbirsoft/ProfileTest.java
@@ -0,0 +1,38 @@
+package com.simbirsoft;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
+import io.grpc.ManagedChannelBuilder;
+import org.junit.jupiter.api.Test;
+import profile.ProfileGrpc;
+import profile.ProfileOuterClass;
+
+import static com.google.protobuf.util.JsonFormat.printer;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProfileTest {
+
+    private JsonFormat.Printer jsonPrinter = printer();
+
+    private ProfileGrpc.ProfileBlockingStub profileBlockingStub = ProfileGrpc.newBlockingStub(
+            ManagedChannelBuilder.forAddress("localhost", 8092)
+                    .usePlaintext()
+                    .build()
+    );
+
+    @Test
+    void profileTest() throws InvalidProtocolBufferException {
+        ProfileOuterClass.Request request = ProfileOuterClass.Request.newBuilder()
+                .addHotelIds("4")
+                .setLocale("US")
+                .build();
+
+        System.out.println("Request: " + jsonPrinter.print(request));
+
+        ProfileOuterClass.Result response = profileBlockingStub.getProfiles(request);
+
+        assertEquals("Hotel Vitale", response.getHotels(0).getName());
+
+        System.out.println("Response " + jsonPrinter.print(response));
+    }
+}
